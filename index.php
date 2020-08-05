ff2b11228fb35197d456e058f7704d8b50a9a3f7
Chamty <ak.cha07.mt@gmail.com>
2020-08-05 23:54:09 +0900

    0805_2

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+if (!empty($_POST)) {
+  if ($_POST['answer_1'] === '') {
+    $error['answer_1'] = 'blank';
+  }
+  if ($_POST['answer_2'] === '') {
+    $error['answer_2'] = 'blank';
+  }
+  if ($_POST['answer_3'] === '') {
+    $error['answer_3'] = 'blank';
+  }
+
+  // if (empty($error)) {
+  //   header('Location: submit.php');
+  //   exit();
+  // }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,22 +29,31 @@
   <div id="container">
     <h1 id="app_title">音楽クイズ</h1>
       <div class="quiz_table">
-      <form action="submit.php" method="get">
+      <form action="" method="post">
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>「ピアノ」の正式名称は？</div>
           <label for="answer_1">回答：</label><input type="text" id="answer_1" name="answer_1">
+          <?php if ($_POST['answer_1'] === ''): ?>
+            <p class="error">※回答を入力してください。</p>
+          <?php endif; ?>
         </div>
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>次のうち、木管楽器なのは？（複数選択可）</div>
           <input type="checkbox" name="answer_2[]" value="サックス">サックス
           <input type="checkbox" name="answer_2[]" value="トロンボーン">トロンボーン
           <input type="checkbox" name="answer_2[]" value="フルート">フルート
+          <?php if ($_POST['answer_2'] === ''): ?>
+            <p class="error">※１つ以上選択してください。</p>
+          <?php endif; ?>
         </div>
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>次のうち、一番大きい楽器は？</div>
           <input type="radio" name="answer_3" value="ヴィオラ">ヴィオラ
           <input type="radio" name="answer_3" value="チェロ">チェロ
           <input type="radio" name="answer_3" value="コントラバス">コントラバス
+          <?php if ($_POST['answer_3'] === ''): ?>
+            <p class="error">※１つ選択してください。</p>
+          <?php endif; ?>
         </div>
         <input type="submit" value="回答する">
       </div>
--- submit.php
+++ submit.php
@@ -13,8 +13,8 @@
       <form action="submit.php" method="get">
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>「ピアノ」の正式名称は？</div>
-          <label for="answer_1">回答：<?php print(htmlspecialchars($_REQUEST['answer_1'], ENT_QUOTES)); ?></label>
-          <?php if($_REQUEST['answer_1'] === 'ピアノフォルテ'): ?>
+          <label for="answer_1">回答：<?php print(htmlspecialchars($_POST['answer_1'], ENT_QUOTES)); ?></label>
+          <?php if($_POST['answer_1'] === 'ピアノフォルテ'): ?>
             <p class="correct">正解！</p>
           <?php else: ?>
             <p class="miss">不正解！</p>
@@ -22,8 +22,24 @@
         </div>
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>木管楽器なのは？</div>
-          <label for="answer_1">回答：<?php print(htmlspecialchars($_REQUEST['answer_2'], ENT_QUOTES)); ?></label>
-          <?php if($_REQUEST['answer_2'] === 'サックス'): ?>
+          <?php $answer_2 = $_POST['answer_2']; ?>
+          <label for="answer_1">回答：
+            <?php 
+              $ok == 0;
+              $ng == 0;
+
+              foreach ($answer_2 as $wood) {
+                print($wood . ' ');
+                if ($wood === 'サックス' || $wood === 'フルート') {
+                  $ok++;
+                } else {
+                  $ng++;
+                }
+            }
+            ?>
+          </label>
+          
+          <?php if($ok == 2 && $ng == 0): ?>
             <p class="correct">正解！</p>
           <?php else: ?>
             <p class="miss">不正解！</p>
@@ -31,8 +47,8 @@
         </div>
         <div class="quiz">
           <div class="question"><span class="qes">Q. </span>一番大きいのは？</div>
-          <label for="answer_1">回答：<?php print(htmlspecialchars($_REQUEST['answer_3'], ENT_QUOTES)); ?></label>
-          <?php if($_REQUEST['answer_3'] === 'コントラバス'): ?>
+          <label for="answer_1">回答：<?php print(htmlspecialchars($_POST['answer_3'], ENT_QUOTES)); ?></label>
+          <?php if($_POST['answer_3'] === 'コントラバス'): ?>
             <p class="correct">正解！</p>
           <?php else: ?>
             <p class="miss">不正解！</p>
